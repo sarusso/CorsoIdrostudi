53b8cf94a6521e7a55b744e3cfbbdf81fd1edea2
Stefano Alberto Russo <stefano.russo@gmail.com>
2022-03-16 08:16:23 +0000

    Hello world e lezione 2

---
+++ lezione2.py
@@ -0,0 +1,8 @@
+
+def sum_list(the_list):
+    totale=0
+    for item in the_list:
+        totale += item
+    return totale
+
+#print(sum_list([1,2,3]))
---
+++ test_matemartica.py
@@ -0,0 +1,16 @@
+from matematica import normalizza as nrm
+
+import numpy as np
+
+import math as mt
+
+np.Array()
+
+
+try:
+    scala
+except:
+    print('scala non definita')
+
+if not normalizza(5) ==  len(range(14)):
+   print('erorre')
